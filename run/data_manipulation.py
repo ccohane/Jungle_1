6d16c657d03f2757d7396118f974123b1583493b
ccohane <ccohane@gmail.com>
2018-12-13 20:53:39 -0500

    Deleted commented out code

--- run/data_manipulation.py
+++ run/data_manipulation.py
@@ -6,23 +6,7 @@ import pandas as pd
 import numpy as np
 import sqlite3
 from sqlalchemy import create_engine
-'''
-df_review_100=pd.read_csv('data/review.csv',nrows =100)
-bus_id = df_review_100['business_id'].unique()
-bus_id_df = pd.DataFrame({'business_id':bus_id })
 
-df_bus = pd.read_csv('less_restaurants_description_in_matrix1.csv')
-result = pd.merge(bus_id_df, df_bus, on = 'business_id')
-result_1 = result.loc[:,['business_id','categories','name','neighborhood','review_count','state','address','city','stars']]
-
-disk_engine = create_engine('sqlite:///jungle.db')
-result_1.to_sql('restaurants', disk_engine, index=False, if_exists='append')
-
-#df = pd.read_csv("data/review.csv",nrows =100)
-#df_review = df.loc[:, ['user_id', 'business_id', 'stars','date', 'text']]
-#df_review.rename(columns = {'text': 'review'}, inplace = True)
-
-'''
 engine = create_engine('sqlite:///Jungle.db')
 df = pd.read_csv("run/description_in_matrix1.csv")
 df = df.drop(['Unnamed: 0'],axis=1)
